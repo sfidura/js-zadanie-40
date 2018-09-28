117c69fbfd592b39b9f588447d87477c95bd7b27
Sebastian Fidura <sebastian@fidura.pl>
2018-09-28 13:44:14 +0200

    gotowe zadanie

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function fCopy(){
+    document.getElementById("name").textContent = document.getElementById("fname").value;
+    
+    document.getElementById("surname").textContent = document.getElementById("fsurname").value;
+    
+    document.getElementById("phone").textContent = document.getElementById("fphone").value;
+}
